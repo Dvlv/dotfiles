15216f3fb7e52922bae90df777036cedc3b6abf3
David Love <Dvlv@users.noreply.github.com>
2019-10-17 15:33:47 +0100

    add pyenv and auto virtualenv sourcing

--- config.fish
+++ config.fish
@@ -6,11 +6,10 @@ begin
 end
 
 . /home/Dvlv/Stuff/easy-git/fish_completion
-/bin/bash /home/Dvlv/bin/ssh_config
+# /bin/bash /home/Dvlv/bin/ssh_config
 
 set PATH /home/Dvlv/bin $PATH
-set PATH /home/Dvlv/go/bin $PATH
-set PATH /home/Dvlv/Downloads/nsq-1.0.0-compat.linux-amd64.go1.8/bin $PATH
+# set PATH /home/Dvlv/Downloads/nsq-1.0.0-compat.linux-amd64.go1.8/bin $PATH
 
 set -x GOPATH /home/Dvlv/go
 
@@ -36,10 +35,24 @@ end
 
 funcsave back
 
+function cd
+    if test -d ./env
+        deactivate
+    end
+
+    builtin cd $argv;
+
+    if test -d ./env
+        source env/bin/activate.fish
+    end
+end
+
 function fish_greeting
-    fortune | cowsay
 end 
 
 funcsave fish_greeting
 
 alias sl='ls'
+
+status --is-interactive; and source (pyenv init -|psub)
+
