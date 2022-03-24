adb65045b066a17889a50517b36c1a7c0ba68ded
David Love <Dvlv@users.noreply.github.com>
2022-03-24 09:48:54 +0000

    fish config

--- config.fish
+++ config.fish
@@ -6,12 +6,15 @@ begin
 end
 
 . /home/Dvlv/Stuff/easy-git/fish_completion
+. /home/Dvlv/Stuff/fnm/fish_completions
 # /bin/bash /home/Dvlv/bin/ssh_config
 
 set PATH /home/Dvlv/bin $PATH
+set PATH $PATH /home/Dvlv/.language-servers
 # set PATH /home/Dvlv/Downloads/nsq-1.0.0-compat.linux-amd64.go1.8/bin $PATH
 
 set -x GOPATH /home/Dvlv/go
+set -x EDITOR nvim
 
 function sourceeg
     set PATH /home/Dvlv/bin $PATH
@@ -44,6 +47,7 @@ function cd
 
     if test -d ./env
         source env/bin/activate.fish
+        set -x PYTHONPATH .
     end
 end
 
@@ -53,7 +57,12 @@ end
 funcsave fish_greeting
 
 alias sl='ls'
+alias ct='ctags -R --exclude="flyway*" --exclude="env" --exclude="web/node_modules/*" --exclude="web/static/*" --exclude="*/__pycache*"'
+alias junior='php artisan'
+alias pt='env SQL_HOST=pg.local pytest -sx -n 4'
 
+fnm env | source
+
+set PATH /home/Dvlv/.pyenv/bin $PATH
 status --is-interactive; and source (pyenv init -|psub)
-
 
