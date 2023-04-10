23976f30521513e72ac1689a2780b4e82aa918a9
Dvlv <Dvlv@users.noreply.github.com>
2023-04-10 08:27:12 +0100

    dconf stuff gnome

---
+++ gnome-workspaces.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+dconf write /org/gnome/desktop/wm/keybindings/close "['<Alt>k']";
+
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-1 "['<Shift><Alt>exclam']";
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-2 "['<Shift><Alt>quotedbl']";
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-3 "['<Shift><Alt>sterling']";
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-4 "['<Shift><Alt>4']";
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-5 "['<Shift><Alt>5']";
+dconf write /org/gnome/desktop/wm/keybindings/move-to-workspace-6 "['<Shift><Alt>6']";
+
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-1 "['<Alt>1']";
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-2 "['<Alt>2']";
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-3 "['<Alt>3']";
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-4 "['<Alt>4']";
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-5 "['<Alt>5']";
+dconf write /org/gnome/desktop/wm/keybindings/switch-to-workspace-6 "['<Alt>6']";
+
+dconf write /org/gnome/desktop/wm/preferences/button-layout "'appmenu:minimize,maximize,close'";
+dconf write /org/gnome/desktop/wm/preferences/focus-mode "'sloppy'";
+dconf write /org/gnome/desktop/wm/preferences/num-workspaces "6";
